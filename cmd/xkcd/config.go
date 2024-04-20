5924d8669208b9b460dcb1257eeda0e87436a848
Vladislav Belogrudov <v.belogrudov@yadro.com>
2024-04-20 20:08:28 +0300

    tune config

--- cmd/xkcd/config.go
+++ cmd/xkcd/config.go
@@ -3,9 +3,9 @@ package main
 import "github.com/ilyakaznacheev/cleanenv"
 
 type Config struct {
-	DatabasePath string `yaml:"db_path" env-default:"database.json"`
-	SourceURL    string `yaml:"source_url" env-default:"https://xkcd.com"`
-	Parallel     int    `jaml:"parallel" env-default:"32"`
+	DatabasePath string `yaml:"db_path" env:"DB" env-default:"database.json"`
+	SourceURL    string `yaml:"source_url" env:"URL" env-default:"https://xkcd.com"`
+	Parallel     int    `jaml:"parallel" env:"PARALLEL" env-default:"32"`
 }
 
 func GetConfig(path string) (Config, error) {
